e3d3ba26456077d48abccf81e1940b0e81d082e8
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-09 00:49:42 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,10 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def cobble
+    puts "Shoe has been repaired"
+    @:condition = 'new'
+  end
+
+end
